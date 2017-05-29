5c5355999383e7d9670a035f7cdad6241265ba26
roman-nevr <roman-nevr@yandex.ru>
2017-05-29 14:41:38 +0300

    queens assignment

--- app/src/test/java/org/berendeev/roma/foodscreen/QueensTest.java
+++ app/src/test/java/org/berendeev/roma/foodscreen/QueensTest.java
@@ -6,31 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class QueensTest {
-    public static final int N = 8;
+    public static final int N = 9;
 
     @Test
     public void solve(){
-        for (int i = 0; i < N; i++) {
-            List<Queen> queens = new ArrayList<>();
-            queens.add(new Queen(i, 0));
-            int rest = 7;
-//            findNextFreePlace(0, i, rest);
+        List<Queen> queens = new ArrayList<>();
+        solve(queens);
+    }
+
+    private void solve(List<Queen> queens){
+        int size = queens.size();
+        if (size == N){
+            printSolution(queens);
+            return;
+        }
+        int y = size;
+        for (int x = 0; x < N; x++) {
+            if (isFreeCell(x, y, queens)){
+                queens.add(new Queen(x, y));
+                solve(queens);
+                queens.remove(size);
+            }
         }
     }
 
-    private void findNextFreePlace(int x, int y, int rest, List<Queen> queens) {
-        while (x < N && y < N){
-            x++;
-            if (x == N){
-                x = 0;
-                y++;
-                if (y == N){
-                    return;
-                }
-            }
-//            if ()
-        }
-    }
+//    private void findNextFreePlace(int x, int y, int rest, List<Queen> queens) {
+//        while (x < N && y < N){
+//            x++;
+//            if (x == N){
+//                x = 0;
+//                y++;
+//                if (y == N){
+//                    return;
+//                }
+//            }
+//            if (isFreeCell(x, y, queens)){
+//                queens.add(new Queen(x, y));
+//                rest--;
+//                if (rest == 0){
+//                    System.out.println(queens);
+//                }else {
+//                    findNextFreePlace(x, y, rest, queens);
+//                }
+//            }
+//        }
+//    }
 
     private boolean isFreeCell(int px, int py, List<Queen> queens){
         boolean isFree = true;
@@ -44,6 +64,27 @@ public class QueensTest {
             }
         }
         return isFree;
+    }
+
+    private void printSolution(List<Queen> queens){
+        for (int i = 0; i < N; i++) {
+            System.out.print("+-");
+        }
+        System.out.println("+");
+        for (int y = 0; y < N; y++) {
+            for (int x = 0; x < N; x++) {
+                if (queens.get(y).x == x){
+                    System.out.print("|W");
+                }else {
+                    System.out.print("|o");
+                }
+            }
+            System.out.println("|");
+        }
+        for (int i = 0; i < N; i++) {
+            System.out.print("+-");
+        }
+        System.out.println("+");
     }
 
     class Queen{
@@ -60,6 +101,10 @@ public class QueensTest {
 
         public int xPlusY(){
             return x + y;
+        }
+
+        @Override public String toString() {
+            return "x = " + x + ", y = " + y;
         }
     }
 }
